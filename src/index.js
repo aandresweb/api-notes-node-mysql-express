fbe972ba04a42643ea3c64008b10188bded90e6b
Andres <agodinezvizcardo@gmail.com>
2022-12-15 01:15:05 -0500

    i've set not found handle error

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ const app = express();
 
 app.use(express.json());
 app.use("/api", notesRoutes);
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Endpoint not found",
+  });
+});
 
 app.listen(3000);
